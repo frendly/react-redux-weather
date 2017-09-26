80051a95dbce4f293bf426ca777ebec899a0bab4
i.popov <i.popov@rambler-co.ru>
2017-09-26 17:37:12 +0300

    formated DashboardItem

--- src/components/DashboardItem/DashboardItem.js
+++ src/components/DashboardItem/DashboardItem.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import './DashboardItem.css';
+import React from "react";
+import "./DashboardItem.css";
 
-const DashboardItem = ({ name, temperature, handleRemove, id, wind, pressure, icon }) => {
+const DashboardItem = ({
+  name,
+  temperature,
+  handleRemove,
+  id,
+  wind,
+  pressure,
+  icon
+}) => {
   const img = `https://openweathermap.org/img/w/${icon}.png`;
   return (
-    <li className='DashboardItem'>
-      <div><strong>{name}</strong></div>
-      <div className="DashboardItem__weather">
-        <span>{icon ? <img src={img} alt='' /> : ''}</span>
-        <span>{temperature > 0 ? '+' : ''} {temperature} °C</span>
+    <li className="DashboardItem">
+      <div>
+        <strong>{name}</strong>
       </div>
-      <div className="DashboardItem__description">{wind ? `Ветер: ${wind} м/с` : ''}</div>
-      <div className="DashboardItem__description">{pressure ? `Давление: ${pressure} мм` : ''}</div>
-      <button className="DashboardItem__close" onClick={() => handleRemove(id)}>✕</button>
+      <div className="DashboardItem__weather">
+        <span>{icon ? <img src={img} alt="" /> : ""}</span>
+        <span>
+          {temperature > 0 ? "+" : ""} {temperature} °C
+        </span>
+      </div>
+      <div className="DashboardItem__description">
+        {wind ? `Ветер: ${wind} м/с` : ""}
+      </div>
+      <div className="DashboardItem__description">
+        {pressure ? `Давление: ${pressure} мм` : ""}
+      </div>
+      <button className="DashboardItem__close" onClick={() => handleRemove(id)}>
+        ✕
+      </button>
     </li>
   );
-}
+};
 
 export default DashboardItem;
