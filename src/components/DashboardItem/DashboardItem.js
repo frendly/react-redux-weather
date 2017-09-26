d5070266589e5af79c80028982e19e3f21ab6326
i.popov <i.popov@rambler-co.ru>
2017-09-26 13:23:17 +0300

    weather icon use https

--- src/components/DashboardItem/DashboardItem.js
+++ src/components/DashboardItem/DashboardItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './DashboardItem.css';
 
 const DashboardItem = ({ name, temperature, handleRemove, id, wind, pressure, icon }) => {
-  const img = `http://openweathermap.org/img/w/${icon}.png`;
+  const img = `https://openweathermap.org/img/w/${icon}.png`;
   return (
     <li className='DashboardItem'>
       <div><strong>{name}</strong></div>
